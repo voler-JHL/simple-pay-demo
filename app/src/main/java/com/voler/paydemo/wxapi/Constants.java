0d3a48b88daa654d7444ccf93fd83429703e04c0
HJL_voler <13039201005@163.com>
2016-08-03 20:30:48 +0800

    初次提交

---
+++ app/src/main/java/com/voler/paydemo/wxapi/Constants.java
@@ -0,0 +1,13 @@
+package com.voler.paydemo.wxapi;
+
+public class Constants {
+	// APP_ID 替换为你的应用从官方网站申请到的合法appId
+    public static final String APP_ID = "wx596816a493629095";
+
+//    public static class ShowMsgActivity {
+//		public static final String STitle = "showmsg_title";
+//		public static final String SMessage = "showmsg_message";
+//		public static final String BAThumbData = "showmsg_thumb_data";
+//	}
+
+}
---
+++ app/src/main/java/com/voler/paydemo/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,54 @@
+package com.voler.paydemo.wxapi;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.tencent.mm.sdk.constants.ConstantsAPI;
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import com.voler.paydemo.R;
+
+
+/**
+ * 微信回调Activity，必须在wxapi下
+ */
+public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
+	
+	private static final String TAG = "WXPayEntryActivity";
+	
+    private IWXAPI api;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pay_result);
+        
+    	api = WXAPIFactory.createWXAPI(this, Constants.APP_ID);
+        api.handleIntent(getIntent(), this);
+    }
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		setIntent(intent);
+        api.handleIntent(intent, this);
+	}
+
+	@Override
+	public void onReq(BaseReq req) {
+	}
+
+	@Override
+	public void onResp(BaseResp resp) {
+		if (resp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
+			//发送广播，为intent添加的String必须一致
+			sendBroadcast(new Intent("com.voler.wxpay_message").putExtra("errorcode", resp.errCode));
+		}
+		WXPayEntryActivity.this.finish();
+	}
+}
